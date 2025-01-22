cd4a583f72b7b4cf3b15e3cf21350a4bb6a330eb
dasBente <dasbente@gmail.com>
2025-01-23 00:58:46 +0100

    add options (arbitrarily picked)

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This repository is supposed to be a collection of portable nvim configs for
 various programming environments, bundled in an easy to execute nix flake format
 for (my own) convenience.
+
+## Configs
+
+- `github:dasbente/portable-nvim#default`: My preferred "maximal" working
+  environment
--- packages/default.nix
+++ packages/default.nix
@@ -1,5 +1,60 @@
-{...}:
+{ pkgs, lib, ... }:
 
 {
+  vim = {
 
+    viAlias = true;
+    vimAlias = true;
+
+    spellcheck.enable = true;
+
+    telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
+
+    lsp = {
+      formatOnSave = true;
+    };
+
+    languages = {
+      enableLSP = true;
+      enableFormat = true;
+      enableTreesitter = true;
+
+      nix.enable = true;
+      markdown.enable = true;
+      bash.enable = true;
+      css.enable = true;
+      html.enable = true;
+      python.enable = true;
+      ts.enable = true;
+      rust.enable = true;
+      rust.crates.enable = true;
+      lua.enable = true;
+      tailwind.enable = true;
+      svelte.enable = true;
+    };
+
+    visuals = {
+      nvim-web-devicons.enable = true;
+      nvim-cursorline.enable = true;
+    };
+
+    filetree.neo-tree.enable = true;
+    treesitter.context.enable = true;
+
+    binds = {
+      whichKey.enable = true;
+      cheatsheet.enable = true;
+    };
+
+    git = {
+      enable = true;
+      gitsigns.enable = true;
+    };
+
+    dashboard.dashboard-nvim.enable = true;
+    projects.project-nvim.enable = true;
+    sessions.nvim-session-manager.enable = true;
+    comments.comment-nvim.enable = true;
+  };
 }
