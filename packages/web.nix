048df6dca839c0cc1c883ad56babb2138ba7457e
dasBente <dasbente@gmail.com>
2025-01-23 17:20:14 +0100

    add web config

---
+++ packages/web.nix
@@ -0,0 +1,50 @@
+{...}: {
+  vim = {
+    viAlias = true;
+    vimAlias = true;
+
+    spellcheck.enable = true;
+
+    telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
+
+    lsp = {
+      formatOnSave = true;
+    };
+
+    languages = {
+      enableLSP = true;
+      enableFormat = true;
+      enableTreesitter = true;
+
+      # add languages for everyday use
+      markdown.enable = true;
+      bash.enable = true;
+      html.enable = true;
+      ts.enable = true;
+      css.enable = true;
+    };
+
+    visuals = {
+      nvim-web-devicons.enable = true;
+      nvim-cursorline.enable = true;
+    };
+
+    filetree.neo-tree.enable = true;
+    treesitter.context.enable = true;
+
+    binds = {
+      whichKey.enable = true;
+      cheatsheet.enable = true;
+    };
+
+    git = {
+      enable = true;
+      gitsigns.enable = true;
+    };
+
+    dashboard.dashboard-nvim.enable = true;
+    projects.project-nvim.enable = true;
+    comments.comment-nvim.enable = true;
+  };
+}
