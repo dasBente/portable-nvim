6011c7f420fd458968707034284758814b27a623
dasBente <dasbente@gmail.com>
2025-11-16 16:26:59 +0000

    fix package config function

--- flake.nix
+++ flake.nix
@@ -6,23 +6,15 @@
     nvf.url = "github:NotAShelf/nvf";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: 
-  let
+  outputs = {nixpkgs, ...} @ inputs: let
     system = "x86_64-linux";
 
-    baseConfig = mod: (inputs.nvf.lib.neovimConfiguration {
-      pkgs = nixpkgs.legacyPackages.${system};
-      modules = [mod];
-    }).neovim;
-
-    config = mod: baseConfig mod // {
-      meta = (baseConfig mod.meta or {}) // {
-        license = nixpkgs.lib.licenses.mit;
-        platforms = [system];
-      };
-    };
-  in
-  {
+    config = mod:
+      (inputs.nvf.lib.neovimConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [mod];
+      }).neovim;
+  in {
     packages."${system}" = {
       default = config ./packages/default.nix;
       web = config ./packages/web.nix;
