29daaf0ccf70730c1874b19a3e8e012032fb4b41
dasBente <dasbente@gmail.com>
2026-03-05 11:29:27 +0100

    update

--- modules/nixosModules/shared-opts.nix
+++ modules/nixosModules/shared-opts.nix
@@ -2,136 +2,141 @@
   self,
   inputs,
   ...
-}: {
-  flake.nixosModules.shared-opts = {pkgs, ...}: {
-    imports = with self.nixosModules; [
-      tools-telescope
-      lang-markdown
-      lang-nix
-      lang-bash
-    ];
+}:
+{
+  flake.nixosModules.shared-opts =
+    { pkgs, ... }:
+    {
+      imports = with self.nixosModules; [
+        tools-telescope
+        lang-markdown
+        lang-nix
+        lang-bash
+      ];
 
-    config = {
-      vim = {
-        viAlias = true;
-        vimAlias = true;
-        spellcheck.enable = true;
-        autocomplete.nvim-cmp.enable = true;
+      config = {
+        vim = {
+          viAlias = true;
+          vimAlias = true;
+          spellcheck.enable = true;
+          autocomplete.nvim-cmp.enable = true;
 
-        clipboard = {
-          enable = true;
-          providers.wl-copy.enable = true;
-          registers = "unnamedplus";
-        };
-
-        lsp = {
-          enable = true;
-          formatOnSave = true;
-        };
-
-        languages = {
-          enableFormat = true;
-          enableTreesitter = true;
-        };
-
-        visuals = {
-          nvim-web-devicons.enable = true;
-          nvim-cursorline.enable = true;
-          indent-blankline.enable = true;
-        };
-
-        utility.oil-nvim = {
-          enable = true;
-          setupOpts = {
-            default_file_explorer = true;
-            columns = ["icon"];
-            skip_confirm_for_simple_edits = true;
+          clipboard = {
+            enable = true;
+            providers.wl-copy.enable = true;
+            registers = "unnamedplus";
           };
+
+          lsp = {
+            enable = true;
+            formatOnSave = true;
+          };
+
+          languages = {
+            enableFormat = true;
+            enableTreesitter = true;
+          };
+
+          visuals = {
+            nvim-web-devicons.enable = true;
+            nvim-cursorline.enable = true;
+            indent-blankline.enable = true;
+          };
+
+          utility.oil-nvim = {
+            enable = true;
+            setupOpts = {
+              default_file_explorer = true;
+              columns = [ "icon" ];
+              skip_confirm_for_simple_edits = true;
+            };
+          };
+
+          treesitter = {
+            enable = true;
+            context.enable = true;
+          };
+
+          binds = {
+            whichKey.enable = true;
+            cheatsheet.enable = true;
+          };
+
+          git = {
+            enable = true;
+            gitsigns.enable = true;
+          };
+
+          dashboard.dashboard-nvim = {
+            enable = true;
+            setupOpts.scope.enabled = true;
+          };
+
+          comments.comment-nvim.enable = true;
+          filetree.neo-tree.enable = true;
+
+          options = {
+            smartindent = true;
+
+            tabstop = 4;
+            shiftwidth = 4;
+            softtabstop = 4;
+          };
+
+          theme = {
+            enable = true;
+            name = "onedark";
+          };
+
+          keymaps = [
+            {
+              key = "-";
+              mode = "n";
+              action = "<cmd>Oil<cr>";
+              desc = "Open parent directory";
+            }
+            {
+              key = "J";
+              mode = "v";
+              action = ":m '>+1<cr>gv=gv";
+            }
+            {
+              key = "K";
+              mode = "v";
+              action = ":m '<-2<cr>gv=gv";
+            }
+            {
+              key = "J";
+              mode = "n";
+              action = "mzJ`z";
+            }
+            {
+              key = "n";
+              mode = "n";
+              action = "<c-d>zz";
+            }
+            {
+              key = "N";
+              mode = "n";
+              action = "<c-u>zz";
+            }
+            {
+              key = "<c-f>";
+              mode = "n";
+              action =
+                let
+                  tmux-sessionizer = inputs.my-bins.packages.${pkgs.system}.tmux-sessionizer;
+                in
+                "<cmd>silent !tmux neww ${tmux-sessionizer}/bin/tmux-sessionizer<cr>";
+            }
+            {
+              key = "<leader>s";
+              mode = "n";
+              action = ":%s/\<<c-r><c-w>\>/<c-r><c-w>/gI<Left><Left><Left>";
+              desc = "Search and Replace hovered";
+            }
+          ];
         };
-
-        treesitter = {
-          enable = true;
-          context.enable = true;
-        };
-
-        binds = {
-          whichKey.enable = true;
-          cheatsheet.enable = true;
-        };
-
-        git = {
-          enable = true;
-          gitsigns.enable = true;
-        };
-
-        dashboard.dashboard-nvim = {
-          enable = true;
-          setupOpts.scope.enabled = true;
-        };
-
-        comments.comment-nvim.enable = true;
-        filetree.neo-tree.enable = true;
-
-        options = {
-          smartindent = true;
-
-          tabstop = 4;
-          shiftwidth = 4;
-          softtabstop = 4;
-        };
-
-        theme = {
-          enable = true;
-          name = "onedark";
-        };
-
-        keymaps = [
-          {
-            key = "-";
-            mode = "n";
-            action = "<cmd>Oil<cr>";
-            desc = "Open parent directory";
-          }
-          {
-            key = "J";
-            mode = "v";
-            action = ":m '>+1<cr>gv=gv";
-          }
-          {
-            key = "K";
-            mode = "v";
-            action = ":m '<-2<cr>gv=gv";
-          }
-          {
-            key = "J";
-            mode = "n";
-            action = "mzJ`z";
-          }
-          {
-            key = "n";
-            mode = "n";
-            action = "<c-d>zz";
-          }
-          {
-            key = "N";
-            mode = "n";
-            action = "<c-u>zz";
-          }
-          {
-            key = "<c-f>";
-            mode = "n";
-            action = let
-              tmux-sessionizer = inputs.my-bins.packages.${pkgs.system}.tmux-sessionizer;
-            in "<cmd>silent !tmux neww ${tmux-sessionizer}/bin/tmux-sessionizer<cr>";
-          }
-          {
-            key = "<leader>s";
-            mode = "n";
-            action = ":%s/\<<c-r><c-w>\>/<c-r><c-w>/gI<Left><Left><Left>";
-            desc = "Search and Replace hovered";
-          }
-        ];
       };
     };
-  };
 }
